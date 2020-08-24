8966cb263591571e869545f678f92c91c072c2a6
Johan Arias <johan.arias@mail.escuelaing.edu.co>
2020-08-23 22:20:37 -0500

    Finish methods lib 2nd part

--- libreria.py
+++ libreria.py
@@ -1,4 +1,5 @@
 import math as m 
+import numpy as np
 
 def sumar(compA,compB):
     
@@ -160,3 +161,143 @@ def fase(comp):
     """
     resultado = m.atan(comp[1]/comp[0])
     return round(resultado,2)
+
+def sumavectores(v,w):
+    if (len(v) != len(w)):
+        raise Exception("Los vectores tienen diferentes longitudes no se pueden sumar")
+    else:
+        rta = []
+        for i in range(len(v)):
+            rta.append(sumar(v[i],w[i]))
+    return rta
+
+def restavectores(v,w):
+    if (len(v) != len(w)):
+        raise Exception("Los vectores tienen diferentes longitudes no se pueden restar")
+    else:
+        rta = []
+        for i in range(len(v)):
+            rta.append(restar(v[i],w[i]))
+    return rta
+
+def inversovector(v):
+     return escalarporvector(-1,v)
+
+def escalarporvector(r,v):
+    for i in range(len(v)):
+        v[i] = ((v[i][0] * r, v[i][1] * r))
+    return v 
+
+def sumarmatrices(M,N):
+    if (len(M) != len(N)):
+        raise Exception("Las matrices no se puede sumar porque tienen tamaños distintos")
+    else:
+        rta = []
+        for i in range(len(M)):
+            rta.append(sumavectores(M[i],N[i]))
+    return rta
+
+def inversomatriz(M):
+    for i in range(len(M)):
+        M[i] = inversovector(M[i])
+    return M
+
+def multiescalarmatriz(r,M):
+    for i in range(len(M)):
+        M[i] = escalarporvector(r,M[i])
+    return M
+
+def transpuestamatriz(M):
+    
+    trans = []
+    for fil in range(len(M[0])):
+        fila = []
+        for col in range(len(M)):
+            fila.append(M[col][fil])
+        trans.append(fila)
+    return trans
+
+def transpuestavector(v):
+    rta = []
+    for comp in v:
+        if  (type(comp) is tuple):
+            rta.append([comp])
+        else:
+            rta.append(comp[0])
+    return rta
+
+def conjugadomatriz(M):
+    for i in range(len(M)):
+        for j in range(len(M[i])):
+            M[i][j] = conjugado(M[i][j])  
+    return M
+
+def conjugadovector(v):
+    rta = []
+    for comp in v:
+        if  (type(comp) is tuple):
+            rta.append(conjugado(comp))
+        else:
+            rta.append([conjugado(comp[0])])
+    return rta
+
+def dagamatriz(M):
+    return conjugadomatriz(transpuestamatriz(M))
+
+def dagavector(v):
+    return (transpuestavector(conjugadovector(v)))
+        
+def multimatrices(M,N):
+
+    if (len(M[0]) != len(N)):
+        raise Exception("Las matrices no tienen tamaños compatibles")
+    else:
+        rta = [[(0,0)] * len(N[0]) for j in range(len(M))]
+        #print(rta)
+        for fila in range(len(M)):
+            for col in range(len(N[fila])):
+                for it in range(len(rta)):
+                    new = multiplicar(M[fila][it],N[it][col])
+                    old = rta[fila][col] 
+                    rta[fila][col] = (new[0]+old[0], new[1]+old[1])
+        return rta
+
+
+
+def accion(M,v):
+    if (len(M[0]) != len(v)):
+        raise Exception("El tamaño de la matriz y el vector no son compatibles") 
+    else: 
+        vector = transpuestavector(v)
+        return multimatrices(M,vector)
+
+def productointernovectores(v,w):
+
+    if (len(v) != len(w)):
+        raise Exception("Los vectores no tienen tamaños compatibles")
+    else:
+        v = dagavector(v)
+        rta = []
+        aux = 0
+        aux1 = 0
+        for i in range(len(v)):
+            rta.append(multiplicar(v[i][0],w[i]))
+
+        for i in range(len(rta)):
+            aux += rta[i][0]
+            aux1 += rta[i][1]
+        return (aux,aux1)
+         
+def norma(v):
+    rta = 0
+    for comp in v:
+        rta += comp[0]**2 + comp[1]**2
+    return round(m.sqrt(rta),2)
+
+
+def calcdistanciaentrevectores(v,w):
+
+    resta = restavectores(v,w)
+    return norma(resta)
+
+
--- pruebas.py
+++ pruebas.py
@@ -62,7 +62,56 @@ class Test_complejos(unittest.TestCase):
 
     def testdeberiacalcfase2(self):
         self.assertEqual(lib.fase((10,7)),0.61)
-    
 
-if __name__ == '__main__':
+    def testdeberiasumarvectores(self):
+        self.assertEqual(lib.sumavectores([(1,3),(2,3),(1,5)],[(5,4),(2,6),(2,5)]),[(6,7),(4,9),(3,10)])
+
+    #def testnodeberiasumarvectores(self):
+     #   self.assertRaises(Exception("Los vectores tienen diferentes longitudes no se pueden sumar"),lib.sumaVectores([(1,3),(1,2),(2,3),(1,5)],[(5,4),(2,6),(2,5)]))
+
+    def testdeberiarestarvectores(self):
+        self.assertEqual(lib.restavectores([(1,3),(2,3),(1,5)],[(5,4),(2,6),(2,5)]),[(-4,-1),(0,-3),(-1,0)])
+
+    def testdeberiasacarinverso(self):
+        self.assertEqual(lib.inversovector([(1,2),(3,-4),(-3,1)]),[(-1,-2),(-3,4),(3,-1)])
+    
+    def testdeberiasumarmatrices(self):
+        self.assertEqual(lib.sumarmatrices([[(5,-1),(2,2)],[(0,3),(3,0)]],[[(1,2),(3,4)],[(5,6),(7,8)]]),[[(6,1),(5,6)],[(5,9),(10,8)]])
+    
+    def testdebeeriasacarinversomatriz(self):
+        self.assertEqual(lib.inversomatriz([[(1,2),(4,5)],[(2,-1),(-4,-2)]]),[[(-1,-2),(-4,-5)],[(-2,1),(4,2)]])
+    
+    def testdeberiacalcularescalarporvector(self):
+        self.assertEqual(lib.multiescalarmatriz(2,[[(1,3),(5,3)],[(1,4),(5,5)]]),[[(2,6),(10,6)],[(2,8),(10,10)]])
+    
+    def testdeberiacalculartranspuestamatriz(self):
+        self.assertEqual(lib.transpuestamatriz([[(1,2),(2,3)],[(5,4),(6,6)]]),[[(1,2),(5,4)],[(2,3),(6,6)]])
+    
+    def testdeberiacalculartranspuestavector(self):
+        self.assertEqual(lib.transpuestavector([(1,2),(1,4)]),[[(1, 2)], [(1, 4)]])
+        self.assertEqual(lib.transpuestavector([[(1,2)],[(1,4)]]),[(1,2),(1,4)])
+
+    def testdeberiacalcularconjugada(self):
+        self.assertEqual(lib.conjugadomatriz([[(1,2),(2,3)],[(5,4),(6,6)]]),[[(1,-2),(2,-3)],[(5,-4),(6,-6)]])
+
+    def testdeberiacalculardaga(self):
+        self.assertEqual(lib.dagamatriz([[(1,2),(2,3)],[(5,4),(6,6)]]),[[(1,-2),(5,-4)],[(2,-3),(6,-6)]])
+
+    def testdeberiamultiplicarmatrices(self):
+        self.assertEqual(lib.multimatrices([[(1,3),(2,3)],[(4,2),(5,1)]],[[(1,2),(2,3),(4,1)],[(1,4),(3,2),(1,1)]]),[[(-15, 16), (-7, 22), (0, 18)], [(1, 31), (15, 29), (18, 18)]])
+
+    def testdeberiacalcularaccion(self):
+        self.assertEqual(lib.accion([[(1,3),(2,3)],[(4,2),(5,1)]],[(1,2),(2,3)]),[[(-10, 17)], [(7, 27)]])
+
+    def testdeberiacalcularproductointerno(self):
+        self.assertEqual(lib.productointernovectores([(1,0),(0,1),(1,-3)],[(2,1),(0,1),(2,0)]),(5,7))
+    
+    def testdeberiacalcularlanorma(self):
+        self.assertEqual(lib.norma([(3.1,7.1),(6.6,-6.0)]),11.81)
+    
+    def testdeberiacalculardistancia(self):
+        self.assertEqual(lib.calcdistanciaentrevectores([(1,2),(2,4),(5,0)],[(0,2),(1,2),(4,0)]),2.65)
+
+    
+if __name__ == '__main__' :
     unittest.main()
